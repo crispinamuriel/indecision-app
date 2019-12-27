f91eaf575bef3b56935d73dc57402c9ccd90dff7
crispinamuriel <crispinamuriel@gmail.com>
2019-12-26 19:26:13 -0500

    added gitignore

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
-"use strict";
-
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 console.log("app.js is running");
 //JSX - JavaScript XML - a javascript syntax extention provided to us by react, way to define our templates (components) and inject our data into those templates
-var template = React.createElement(
-  "h1",
-  null,
-  "Working as intended"
-);
-var appRoot = document.getElementById("app");
+const template = <h1>Working as intended</h1>;
+const appRoot = document.getElementById("app");
+
 
 ReactDOM.render(template, appRoot);
