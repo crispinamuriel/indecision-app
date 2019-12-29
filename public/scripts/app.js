07e46416ed97361399e2971f87b4cf2f6cad9c22
crispinamuriel <crispinamuriel@gmail.com>
2019-12-29 15:22:17 -0500

    feat: conditional logic to render elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,77 @@
-"use strict";
+'use strict';
 
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-console.log("app.js is running");
-//JSX - JavaScript XML - a javascript syntax extention provided to us by react, way to define our templates (components) and inject our data into those templates
-var template = React.createElement(
-  "h1",
-  null,
-  "Working as intended"
-);
-var appRoot = document.getElementById("app");
+console.log('app.js is running');
+// JSX - JavaScript XML - a javascript syntax extention provided to us by react, way to define our templates (components) and inject our data into those templates
 
-ReactDOM.render(template, appRoot);
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
+
+var userName = 'Miki';
+var userAge = 36;
+var userLocation = 'New York';
+var user = {
+  name: 'Jason',
+  age: 30,
+  location: 'Philadelphia'
+};
+
+function getLocation(location) {
+  return location ? React.createElement(
+    'p',
+    null,
+    'Location: ',
+    location
+  ) : null;
+}
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-console.log("app.js is running");
-//JSX - JavaScript XML - a javascript syntax extention provided to us by react, way to define our templates (components) and inject our data into those templates
-const template = <h1>Working as intended</h1>;
-const appRoot = document.getElementById("app");
+console.log('app.js is running');
+// JSX - JavaScript XML - a javascript syntax extention provided to us by react, way to define our templates (components) and inject our data into those templates
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+}
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+  );
+
+const userName = 'Miki';
+const userAge = 36;
+const userLocation = 'New York';
+const user = {
+  name: 'Jason',
+  age: 30,
+  location: 'Philadelphia'
+}
+
+function getLocation(location) {
+  return location ? <p>Location: {location}</p> : null;
+}
+
+const template2 = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+   {getLocation(user.location)}
+  </div>
+);
+const appRoot = document.getElementById('app');
 
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
